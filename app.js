697a6ce079ce28fd61d20203266d02ced389f1f5
jccrosby <john@realeyes.com>
2013-06-27 14:30:42 -0600

    added data and loaded data

--- app.js
+++ app.js
@@ -8,6 +8,17 @@
 		
 		initialize: function() {
 			console.log('App::initialize()');
+			
+			$.ajax('data/mydata.json', {
+				error: function(jqXHR, status, error){
+					console.log('ERROR: ', error);
+				},
+				success: function(data, status, jqXHR){
+					console.log('data', data);
+				}
+			});
+				
+			
 		}
 		
 	});
